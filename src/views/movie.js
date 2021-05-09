2647a3c77a7ab1bf9cdc0607ebb0ac03c9fcc049
Elenalvarez <81432401+Elenalvarez@users.noreply.github.com>
2021-05-09 18:57:35 +0200

    botón y título cambiados

--- src/views/movie.js
+++ src/views/movie.js
@@ -8,19 +8,29 @@ import {
 import React, { useState, useEffect } from "react";
 
 const useStyles = makeStyles((theme) => ({
-  Typography: { padding: theme.spacing(2) },
-  Button: {
-   display: "inline",
-   fontSize: 40,
-   color: theme.palette.secondary.contrastText,
- 	 textAlign: "center",
- 	 userSelect: "none",
- 	 backgroundColor: "transparent",
- 	 border: "hidden",
- 	 padding: theme.spacing(2),
- 	 lineHeight: 1.5,
- 	 borderRadius: 0.25,
+  Typography: { 
+    padding: theme.spacing(2),
+    fontFamily: "Times New Roman",
+    textAlign: "center",
+    textDecoration: "underline",
+    fontSize: 50,
   },
+    
+  Button: {
+    display: "inline",
+    fontSize: 20,
+    textcolor: theme.palette.secondary.contrastText,
+ 	  textAlign: "center",
+ 	  userSelect: "none",
+ 	  backgroundColor: "transparent",
+ 	  border: "solid lightblue",
+ 	  padding: theme.spacing(2),
+ 	  lineHeight: 0.75,
+ 	  borderRadius: 0.15,
+ 	  padding: 10,
+ 	  margin: 10,
+  },
+  
 }));
 function Movie() {
   const [valor, setValor] = useState("");
@@ -43,6 +53,83 @@ function Movie() {
     setMovie(await respuesta.json());
     console.log(movie);
   }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  if (movie.Response == "False") {
+    return (
+      <Box>
+        <Typography align="center" variant="h1">
+          {movie.Error}
+        </Typography>
+        <Container>
+          <input
+            onChange={(evento) => {
+              setValor(evento.target.value);
+            }}
+            value={valor}
+            name="test"
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              fetchData();
+            }}
+          >
+            show
+          </Button>
+        </Container>
+      </Box>);
+  } else {
+    return (
+      <Box>
+        <Typography class ={classes.Typography}align="center" variant="h1">
+          {movie.Title}
+        </Typography>
+        <Container>
+          <input
+            onChange={(evento) => {
+              setValor(evento.target.value);
+            }}
+            value={valor}
+            name="test"
+          />
+          <Button 
+           	class={classes.Button}
+           	variant="contained"
+            color="primary"
+            onClick={() => {
+              fetchData();
+            }}
+          >
+            show
+          </Button>
+        </Container>
+
+        <Container>
+          <img src={movie.Poster}></img>
+          <Typography align="center"> Actors: {movie.Actors}</Typography>
+          <Typography align="center"> Country: {movie.Country}</Typography>
+          <Typography align="center"> Plot: {movie.Plot}</Typography>
+          <Typography align="center">
+            {" "}
+            Production: {movie.Production}
+          </Typography>
+          <Typography align="center"> Writer: {movie.Writer}</Typography>
+          <Typography align="center"> Released: {movie.Released}</Typography>
+          <Typography align="center">
+            {" "}
+            imdbRating: {movie.imdbRating}
+          </Typography>
+        </Container>
+      </Box>
+    );
+  }
+}
+
+export default Movie;
 
   useEffect(() => {
     fetchData();
