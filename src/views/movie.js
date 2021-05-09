6e7cad179a3ee8ef9eb9f2ee00790e5ba666e630
Elenalvarez <81432401+Elenalvarez@users.noreply.github.com>
2021-05-09 20:10:51 +0200

    Update movie.js

--- src/views/movie.js
+++ src/views/movie.js
@@ -3,6 +3,7 @@ import {
   Box,
   Typography,
   Container,
+  Grid,
   Button,
 } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
@@ -20,16 +21,20 @@ const useStyles = makeStyles((theme) => ({
     display: "inline",
     fontSize: 20,
     textcolor: theme.palette.secondary.contrastText,
- 	  textAlign: "center",
- 	  userSelect: "none",
- 	  backgroundColor: "transparent",
- 	  border: "solid lightblue",
- 	  padding: theme.spacing(2),
- 	  lineHeight: 0.75,
- 	  borderRadius: 0.15,
- 	  padding: 10,
- 	  margin: 10,
+ 	 textAlign: "center",
+ 	 userSelect: "none",
+ 	 backgroundColor: "transparent",
+ 	 border: "solid lightblue",
+ 	 padding: theme.spacing(2),
+ 	 lineHeight: 0.75,
+ 	 borderRadius: 0.15,
+ 	 padding: 10,
+ 	 margin: 10,
   },
+  
+  Grid: {
+	 marginTop: "10%",
+  }
   
 }));
 function Movie() {
@@ -108,99 +113,18 @@ function Movie() {
           </Button>
         </Container>
 
-        <Container>
+        <Box justifyContent="space-around" display="flex">
           <img src={movie.Poster}></img>
+          <Grid class={classes.Grid}>
           <Typography align="center"> Actors: {movie.Actors}</Typography>
           <Typography align="center"> Country: {movie.Country}</Typography>
           <Typography align="center"> Plot: {movie.Plot}</Typography>
-          <Typography align="center">
-            {" "}
-            Production: {movie.Production}
-          </Typography>
+          <Typography align="center"> Production: {movie.Production}</Typography>
           <Typography align="center"> Writer: {movie.Writer}</Typography>
           <Typography align="center"> Released: {movie.Released}</Typography>
-          <Typography align="center">
-            {" "}
-            imdbRating: {movie.imdbRating}
-          </Typography>
-        </Container>
-      </Box>
-    );
-  }
-}
-
-export default Movie;
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-  if (movie.Response == "False") {
-    return (
-      <Box>
-        <Typography align="center" variant="h1">
-          {movie.Error}
-        </Typography>
-        <Container>
-          <input
-            onChange={(evento) => {
-              setValor(evento.target.value);
-            }}
-            value={valor}
-            name="test"
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => {
-              fetchData();
-            }}
-          >
-            show
-          </Button>
-        </Container>
-      </Box>);
-  } else {
-    return (
-      <Box>
-        <Typography align="center" variant="h1">
-          {movie.Title}
-        </Typography>
-        <Container>
-          <input
-            onChange={(evento) => {
-              setValor(evento.target.value);
-            }}
-            value={valor}
-            name="test"
-          />
-          <Button
-           	class={classes.Button}
-            variant="contained"
-            color="primary"
-            onClick={() => {
-              fetchData();
-            }}
-          >
-            show
-          </Button>
-        </Container>
-
-        <Container>
-          <img src={movie.Poster}></img>
-          <Typography align="center"> Actors: {movie.Actors}</Typography>
-          <Typography align="center"> Country: {movie.Country}</Typography>
-          <Typography align="center"> Plot: {movie.Plot}</Typography>
-          <Typography align="center">
-            {" "}
-            Production: {movie.Production}
-          </Typography>
-          <Typography align="center"> Writer: {movie.Writer}</Typography>
-          <Typography align="center"> Released: {movie.Released}</Typography>
-          <Typography align="center">
-            {" "}
-            imdbRating: {movie.imdbRating}
-          </Typography>
-        </Container>
+          <Typography align="center"> imdbRating: {movie.imdbRating}</Typography>
+          </Grid>
+        </Box>
       </Box>
     );
   }
